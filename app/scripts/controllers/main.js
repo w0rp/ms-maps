8a910086958ba0db3eb628acbf9b86d44326eaeb
Ben <ben.may@wazoku.com>
2016-12-12 14:58:56 +0000

    Added map directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,34 @@
  */
 angular.module('msMapsApp.main', [
   'msMapsApp.markers',
+  'msMapsApp.directives.map',
 ])
-.controller('MainCtrl', function(markers) {
+.controller('MainCtrl', function($scope, markers) {
   'use strict'
+  /* global google */
 
   var vm = this
 
   vm.markers = markers
+  vm.homeLocation = {lat: 51.56, lng: -0.25}
+
+  vm.runAddressSearch = () => {
+    console.log('running address search')
+
+    const geocoder = new google.maps.Geocoder()
+
+    geocoder.geocode({address: vm.address}, (results, status) => {
+      console.log(status)
+
+      if (status === 'OK') {
+        const location = results[0].geometry.location
+
+        vm.homeLocation = {lat: location.lat(), lng: location.lng()}
+      } else {
+        /* Show an error here? */
+      }
+
+      $scope.$digest()
+    })
+  }
 })
---
+++ app/scripts/directives/map.js
@@ -0,0 +1,112 @@
+angular.module('msMapsApp.directives.map', [])
+/* global d3 */
+/* global google */
+/* global L */
+
+.directive('googleMapsVisualisation', () => ({
+  restrict: 'E',
+  scope: {
+    val: '=',
+    homeLocation: '=',
+  },
+  link: function(scope, element, attrs) {
+    function getCenter(coordsInput) {
+      var totalLng = 0
+      var totalLat = 0
+      Object.keys(coordsInput).forEach((key) => {
+        totalLng += coordsInput[key][0]
+        totalLat += coordsInput[key][1]
+      })
+
+      return [totalLat / Object.keys(coordsInput).length, totalLng / Object.keys(coordsInput).length]
+    }
+
+    function getZoom(coordsInput) {
+      var bounds = new google.maps.LatLngBounds()
+      Object.keys(coordsInput).forEach((key) => {
+        bounds.extend(new google.maps.LatLng(coordsInput[key][1], coordsInput[key][0]))
+      })
+
+      return bounds
+    }
+
+    function getDistance(from, to) {
+      const distance = google.maps.geometry.spherical.computeDistanceBetween(
+        new google.maps.LatLng(from.lat, from.lng),
+        new google.maps.LatLng(to.lat, to.lng)
+      )
+
+      return distance
+    }
+
+    const mapLocations = {}
+
+    function mapRender(newMapData) {
+      var coords = {}
+
+      var map = new google.maps.Map(d3.select(element[0]).node(), {
+        zoom: 8,
+        center: new google.maps.LatLng(51.56, -0.25),
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+      })
+
+      let lastWindowOpen = null
+
+      Object.keys(newMapData).forEach((itemKey) => {
+        var item = newMapData[itemKey]
+
+        const location = {}
+        mapLocations[itemKey] = location
+
+        location.htmlTemplate = item.bubble
+        location.infoWindow = new google.maps.InfoWindow({content: ''})
+        location.marker = new google.maps.Marker({
+          position: {lat: item.lat, lng: item.lng},
+          map: map,
+          title: 'Uluru (Ayers Rock)',
+        })
+
+        location.marker.addListener('click', () => {
+          if (lastWindowOpen != null) {
+            lastWindowOpen.close()
+          }
+
+          const metersDistance = getDistance(scope.homeLocation, item)
+
+          location.infoWindow.setContent(item.bubble.replace(
+            '!miles',
+            (metersDistance / 1000).toFixed(2) + 'km'
+          ))
+
+          location.infoWindow.open(map, location.marker)
+
+          lastWindowOpen = location.infoWindow
+        })
+      })
+    }
+
+    mapRender(scope.val)
+
+    function updateInfoWindows() {
+      Object.keys(mapLocations).forEach(itemKey => {
+        const location = mapLocations[itemKey]
+        const locationCoords = {
+          lat: location.marker.getPosition().lat(),
+          lng: location.marker.getPosition().lng(),
+        }
+        const metersDistance = getDistance(scope.homeLocation, locationCoords)
+
+        location.infoWindow.setContent(location.htmlTemplate.replace(
+          '!miles',
+          (metersDistance / 1000).toFixed(2) + 'km'
+        ))
+      })
+    }
+
+    updateInfoWindows()
+
+    scope.$watch('homeLocation', () => {
+      updateInfoWindows()
+    })
+  },
+}))
